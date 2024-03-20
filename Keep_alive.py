187c6c33de15e558b03c0b1809255d05857dcce1
yassine0335 <89081376+yassine0335@users.noreply.github.com>
2024-03-21 00:43:45 +0100

    Update Keep_alive.py

--- Keep_alive.py
+++ Keep_alive.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 from threading import Thread
 
 app = Flask("")
 @app.route("/")
-def home():
-return "Bot is logged"
+def index():
+  return "Bot is logged"
 
 def run():
 app.run(host="0.0.0.0", port=8080)
